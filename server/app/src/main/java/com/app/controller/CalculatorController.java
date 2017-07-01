9d8c5fee933539f8107eba757f42f3051860ca43
Ivange Larry <ivangelarry@gmail.com>
2017-07-01 13:40:13 +0100

    Fixed CORS issue and completed client

--- server/app/src/main/java/com/app/controller/CalculatorController.java
+++ server/app/src/main/java/com/app/controller/CalculatorController.java
@@ -2,18 +2,16 @@ package com.app.controller;
 
 import com.app.Operation;
 import com.app.Result;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/calculator")
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public Result calculate(@RequestBody Operation operation) {
         return operation.compute();
     }
